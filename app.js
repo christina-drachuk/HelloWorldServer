3b543968143c065c39f7ddc91c41d464a73913e5
christina-drachuk <cdrachuk@gmail.com>
2023-04-30 21:15:51 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// set up the server
+const express = require("express")
+const app = express()
+const port = 3000
+const logger = require("morgan")
+
+// middleware
+app.use(logger("dev"))
+
+// define middleware that serves static resources in the public directory
+app.use(express.static(__dirname + '/public'))
+
+// start the server
+app.listen(port, () => {
+    console.log(`App server listening on ${port}`)
+})
+
+// define a route for the index
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+// define a route for the homepage
+app.get( "/homepage", ( req, res ) => {
+    res.sendFile(__dirname + "/views/homepage.html");
+})
+
+// define a route for the info page
+app.get( "/info", ( req, res ) => {
+    res.sendFile(__dirname + "/views/info.html");
+} );
+
